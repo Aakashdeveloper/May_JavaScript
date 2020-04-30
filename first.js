841a50eecaa945ca43f4c0c66b1df192ccf7f1f3
aakashDeveloper <ahanda205@gmail.com>
2020-04-30 03:14:31 +0100

    a

---
+++ first.js
@@ -0,0 +1,141 @@
+Es5
+Ecam Script (Baselib {standard})
+Es6
+Es7
+
+///////////
+DataTypes
+/////////
+String  = "Hi" "1" "true" 'df' '3324' 'true' "sdge' 'ffe"
+Number =   88   468468  595488.46464  .002
+Boolean = true/false 
+
+
+Declare
+//Es5
+var a = "Hiiii"
+var b = "334"
+var c = 837
+var d = 87.54
+var e = true
+
+typeof(a)
+"string"
+typeof(b)
+"string"
+typeof(c)
+"number"
+typeof(d)
+"number"
+typeof(e)
+"boolean"
+typeof("hiii")
+"string"
+
+///////////////
+var a = 10
+var b = 5
+undefined
+a+b
+15
+a-b
+5
+a*b
+50
+a/b
+2
+a%b(modules)
+0
+4%2
+0
+4%3
+1
+
+2%4
+2
+3%4
+3
+5%4
+1
+4%5
+4
+
+//Lower%higher > Lower
+//higher%lower > actual rem
+
+String+String = String(Concat)
+Number+String = String
+String+Number = String
+Number+Number = Number
+
+var a ="Hii "
+var b ="JavaScript"
+
+var a ="Hii "
+var b ="JavaScript"
+undefined
+a+b
+"Hii JavaScript"
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+a+c
+"Hii 1"
+a-c
+NaN
+var a = "hi"
+undefined
+var b = true
+undefined
+a+b
+"hitrue"
+a-b
+NaN
+
+"10"+10+10
+"101010"
+10+10+"10"
+"2010"
+10+"10"+10
+"101010"
+
+"10"+10+10-1
+101009
+10+10+"10"-1
+2009
+10+"10"+10-1
+101009
+
+
+"1"+"1"
+"11"
+"a"-"1"
+NaN
+"1"-"1"
+0
+"1"*"3"
+3
+"5"/"10"
+0.5
+"5a"/"10"
+NaN
+"5.01"/"10"
+0.501
+
+4645737567
+
+var a = 4645737567
+undefined
+typeof(a)
+"number"
+a.toString()
+"4645737567"
+var a = 4645737567
+undefined
+a+""
+"4645737567"
+""+a
+"4645737567"
