e07af17aa05f6f8c77beab0b0716a1a7629c1716
aakashDeveloper <ahanda205@gmail.com>
2020-05-05 03:19:53 +0100

    a

---
+++ nine.js
@@ -0,0 +1,17 @@
+var a = 10
+var b = "my age is "+a
+b
+"my age is 10"
+
+//es5
+var name="Avengers",
+var rating=4.5
+var language = "English"
+
+var out="This is movies "+name+" and rating of movies is "+rating+" and in "+language+" language."
+
+//Es6(string literials)
+var out = `This is movies ${name} and rating of movies is ${rating} and in ${language} language.`
+
+out
+"This is movies Avengers and rating of movies is 4.5 and in English language."
