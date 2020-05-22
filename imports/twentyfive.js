4ddd9b53b72106f541fe5dc2f81a401430083d9e
aakashDeveloper <ahanda205@gmail.com>
2020-05-22 03:22:56 +0100

    a

---
+++ imports/twentyfive.js
@@ -0,0 +1,36 @@
+document.getElementsByTagName('h3')
+HTMLCollection(3) [h3, h3, h3]0: h31: h32: h3length: 3__proto__: HTMLCollection
+document.getElementsByTagName('h3')[0]
+<h3>​Crack Interview And Be Industry Ready​</h3>​
+document.getElementsByTagName('h3')[0].innerText="Prepare for interview"
+"Prepare for interview"document.getElementsByTagName('h3')
+HTMLCollection(3) [h3, h3, h3]0: h31: h32: h3length: 3__proto__: HTMLCollection
+document.getElementsByTagName('h3')[0]
+<h3>​Crack Interview And Be Industry Ready​</h3>​
+document.getElementsByTagName('h3')[0].innerText="Prepare for interview"
+"Prepare for interview"
+
+document.getElementsByClassName('text')
+HTMLCollection(4) [div.text, div.text, div.text, div.text]0: div.text1: div.text2: div.text3: div.textlength: 4__proto__: HTMLCollection
+document.getElementsByClassName('text')[1]
+<div class=​"text">​…​</div>​<h4>​<strong>​Multiple Mock Interview​</strong>​</h4>​<p>​…​</p>​</div>​
+document.getElementsByClassName('text')[1].innerText
+"Multiple Mock Interview
+
+Mock Interview Form Industry Expertes"
+document.getElementsByClassName('text')[1].innerText="Many Mock Interview"
+"Many Mock Interview"
+
+document.getElementsByTagName('h3')[0].style.color="red"
+"red"
+document.getElementsByTagName('h3')[0].style.color="blue"
+"blue"
+document.getElementsByTagName('h3')[0].style.fontSize=40
+40
+document.getElementsByTagName('h3')[0].style.fontSize="40px"
+"40px"
+document.getElementsByTagName('h3')[0].style.visibility="hidden"
+"hidden"
+document.getElementsByTagName('h3')[0].style.visibility="visible"
+"visible"
+document.getElementsByTagName('h1')[0].innerText="I m from nareshit"
