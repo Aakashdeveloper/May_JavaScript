964bdb49674a67f7c28c95f1b58dff76a450dd25
aakashDeveloper <ahanda205@gmail.com>
2020-05-29 07:40:48 +0530

    a

---
+++ jquery/first.js
@@ -0,0 +1,11 @@
+$('h2')
+init(2) [h2, h2, prevObject: init(1), context: document, selector: "h2"]0: h21: h2context: documentlength: 2prevObject: init [document, context: document]selector: "h2"__proto__: Object(0)
+$('h2')[0].innerText="Developerfunnel"
+"Developerfunnel"
+
+$('.classname')
+init [prevObject: init(1), context: document, selector: ".classname"]
+$('#idname')
+init {context: document, selector: "#idname"}
+$('.name').value
+undefined
